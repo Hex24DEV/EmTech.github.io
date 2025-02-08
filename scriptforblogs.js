5184a1b437d8dd4f1855e71bd10c72c52ff7bc5f
Hex24DEV <Mathewsagsagat@gmail.com>
2025-02-08 19:26:27 +0800

    Add files via upload

---
+++ scriptforblogs.js
@@ -0,0 +1,5 @@
+let blogs = [
+  { title: "Blog Post #1", link: "blogs/blog1.html" },
+  { title: "Blog Post #2", link: "blogs/blog2.html" },
+  { title: "Blog Post #3", link: "blogs/blog3.html" }
+];
