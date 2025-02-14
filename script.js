f855637e5642d3382a3c5a3a1f2e8f5ee08c078c
Hex24DEV <Mathewsagsagat@gmail.com>
2025-02-14 11:00:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 const blogLinks = document.getElementById("blog-links");
 const blogs = [
   { title: "Blog Post #1", link: "blogs/blog1.html" },
-  { title: "Blog Post #2", link: "blogs/blog2.html" }, // Example blog
-  { title: "Blog Post #3", link: "blogs/blog3.html" }  // Example blog
+  { title: "Blog Post #2", link: "blogs/blog2.html" },
+  { title: "Blog Post #3", link: "blogs/blog3.html" }
+  { title: "Blog Post #4", link: "blogs/blog4.html" }
+  { title: "Blog Post #5", link: "blogs/blog5.html" }
 ];
 
 // Populate blog list dynamically
