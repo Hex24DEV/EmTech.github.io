2f0607a88b30d9bdbef0f486a3baa7f097906231
Hex24DEV <Mathewsagsagat@gmail.com>
2025-02-14 10:59:35 +0800

    Update scriptforblogs.js

--- scriptforblogs.js
+++ scriptforblogs.js
@@ -2,4 +2,6 @@ let blogs = [
   { title: "Blog Post #1", link: "blogs/blog1.html" },
   { title: "Blog Post #2", link: "blogs/blog2.html" },
   { title: "Blog Post #3", link: "blogs/blog3.html" }
+  { title: "Blog Post #4", link: "blogs/blog4.html" }
+  { title: "Blog Post #5", link: "blogs/blog5.html" }
 ];
